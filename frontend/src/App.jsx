cb0080e2c20805d05fefaeb9f4c994aacd53d113
Gouravjoshi986 <gourav.joshi2003@gmail.com>
2024-04-07 17:24:38 +0530

    Dashboard Page and Send Money Page Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import SignUp from "./pages/Signup"
 import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
 
 function App(){
   return (
@@ -9,8 +11,8 @@ function App(){
         <Routes>
           <Route path="/signup" element={<SignUp/>}/>
           <Route path="/signin" element={<Signin/>}/>
-          <Route path="/signup" element={<SignUp/>}/>
-          <Route path="/signup" element={<SignUp/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/send" element={<SendMoney/>}/>
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -5,6 +5,7 @@ import InputBox from "../components/InputBox";
 import SubHeading from "../components/SubHeading";
 import {useNavigate} from "react-router-dom"
 import { useState } from "react";
+import axios from "axios"
 
 const Signin = () =>{
     const [username, setUserName] = useState("");
