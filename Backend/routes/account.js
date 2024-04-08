9fd2bc5aff0352c12306b5c186906bf9ff8af5af
Gouravjoshi986 <gourav.joshi2003@gmail.com>
2024-04-08 16:21:33 +0530

    Bug Removal from Send Money route

--- Backend/routes/account.js
+++ Backend/routes/account.js
@@ -1,45 +1,46 @@
 const express = require('express');
 const { authMiddleware } = require('../middleware')
 const { Account } = require('../db');
-const mongoose = require('mongoose')
+const { default: mongoose } = require('mongoose');
 
 const router = express.Router();
 
-router.get('/balance',authMiddleware,async (req,res)=>{
+router.get('/balance', authMiddleware, async (req, res) => {
     try {
-        const account = await Account.findOne({userId:req.userId})
-        if(!account){
+        const account = await Account.findOne({ userId: req.userId });
+        if (!account) {
             return res.status(404).json({
-                message:"NO account found for the requested user Id"
+                message: "No Account found for the requested User ID."
             });
         }
-        res.json({balance:account.balance});
-    } catch (err) {
-        res.status(500).json({message:"Internal Server Error"})
+        res.json({ balance: account.balance });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
     }
 })
 
-router.post('/transfer',authMiddleware,async (req,res)=>{
+router.post('/transfer', authMiddleware, async (req, res) => {
     try {
-        const mongooseSession = mongoose.startSession();
+        const mongooseSession = await mongoose.startSession();
         mongooseSession.startTransaction();
-        const {amount,to} = req.body;
+        const { amount, to } = req.body;
 
-        const account = await Account.findOne({userId:req.userID}).session(mongooseSession);
-        if(!account){
+        const account = await Account.findOne({ userId: req.userId }).session(mongooseSession);
+        if (!account) {
             await mongooseSession.abortTransaction();
             return res.status(404).json({
                 message: "No User/Account Found"
             });
         }
-        if(account.balance<amount){
+        if (account.balance < amount) {
             await mongooseSession.abortTransaction();
             return res.status(400).json({
                 message: "Insufficient Funds"
             })
         }
 
-        const toAccount = Account.findOne({userId:to}).session(mongooseSession);
+        const toAccount = await Account.findOne({ userId: to }).session(mongooseSession);
         if (!toAccount) {
             await mongooseSession.abortTransaction();
             return res.status(400).json({
@@ -54,12 +55,8 @@ router.post('/transfer',authMiddleware,async (req,res)=>{
         res.json({
             message: "Transfer successful"
         });
-
-    } catch (err) {
-        res.status(500).json({
-            message:"Internal Server Error"
-        });
+    } catch (error) {
+        console.error(error);
     }
-});
-
-module.exports = router
+})
+module.exports = router;
